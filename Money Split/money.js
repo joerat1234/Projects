1444c26d59095977968ecd79ee2228f773d88f85
Joe Ratcliffe <joffa1234@hotmail.co.uk>
2024-03-03 17:02:35 +1300

    Added function for adding names to UL
    Unsure whether to use a UL or not but trailing this for now. Have left in original code which did not use a list

---
+++ Money Split/money.js
@@ -0,0 +1,40 @@
+// function saveName(){
+//     var nameInput = document.getElementById("nameInput").value;
+//     var user1 = {};
+//     user1.nameInput = nameInput;
+
+//     var outputDiv = document.getElementById('outputDiv');
+//     outputDiv.innerHTML = "Name: " + user1.nameInput;
+
+// Above is the original idea but when trying to add a second person, it overwrites the first person you input.
+// }
+
+const nameInput = document.getElementById("name-input");
+const listContainer = document.getElementById("list-container");
+
+function saveName(){
+    if(nameInput.value === ""){
+        alert("Please add a name!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = nameInput.value
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    nameInput.value = "";
+    saveData();    
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
